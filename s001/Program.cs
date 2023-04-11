5b607e19f969587863ab3b1e9152c80264ab1e67
sen <kseniya2495@yandex.ru>
2023-04-11 15:13:29 +0300

    задача 1

--- s001/Program.cs
+++ s001/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимает два числа и проверяет является ли первое квадратом второго
+Console.Write("a=");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b=");
+int b = int.Parse(Console.ReadLine());
+if (a == b*b)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
