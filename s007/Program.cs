f4179a39c3dbd7ddf1912e16d4a2bbffadc6141c
sen <kseniya2495@yandex.ru>
2023-04-12 14:54:16 +0300

    задача 7(дополнение)

--- s007/Program.cs
+++ s007/Program.cs
@@ -1,4 +1,11 @@
 ﻿// на вход трехзначное число, на выход последняя цифра этого числа
-Console.Write("введите трехзначное число ");
+Console.Write("введите число ");
 int number = int.Parse(Console.ReadLine());
-Console.WriteLine (number % 10);
+if (number >= 100 && number < 1000)
+{
+    Console.WriteLine (number % 10);
+}
+else
+{
+    Console.WriteLine ("число не подходит");
+}
