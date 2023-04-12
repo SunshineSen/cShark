ec325e667d7f103acce7ba9c17cd60df4bc4d2be
sen <kseniya2495@yandex.ru>
2023-04-12 15:33:22 +0300

    задача 6

---
+++ s006/Program.cs
@@ -0,0 +1,13 @@
+﻿// на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Write("введите число ");
+int a = int.Parse(Console.ReadLine());
+int b = a % 2;
+
+if (b == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
