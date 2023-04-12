a37af025fd235a5eb0f4cb5074802a722d3c7f43
sen <kseniya2495@yandex.ru>
2023-04-12 14:44:33 +0300

    задача 7

---
+++ s007/Program.cs
@@ -0,0 +1,4 @@
+﻿// на вход трехзначное число, на выход последняя цифра этого числа
+Console.Write("введите трехзначное число ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine (number % 10);
