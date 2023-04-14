377709b1c5121e1be6141730e3f5abf95a2cf0b6
SunshineSen <128902151+SunshineSen@users.noreply.github.com>
2023-04-14 15:20:51 +0300

    Delete s005 directory

--- s005/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// на вход принимает N, на выходе показывает целые числа от -N до N
-Console.Write("введите число ");
-int j = int.Parse(Console.ReadLine());
- for (int i = -j; i <= j; i++)
- {
-        Console.WriteLine(i+"");
-
- }
