205b265b50b1dd101bd11095bde9aeac7fc020d7
sen <kseniya2495@yandex.ru>
2023-04-14 16:33:31 +0300

    рандом 10-99

---
+++ sem2/s009/Program.cs
@@ -0,0 +1,13 @@
+﻿// случайное число из отрезка 10-99 и показывает наибольшую цифру числа
+int number = new Random().Next(10, 100);
+int first = number/10;
+int second = number % 10;
+System.Console.WriteLine(number);
+if (first > second)
+{
+    System.Console.WriteLine(first);
+}
+else
+{
+    System.Console.WriteLine(second);
+}
