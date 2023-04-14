262c1f431ae89c98e69dfb0d00c23f499d6315a1
sen <kseniya2495@yandex.ru>
2023-04-14 17:26:50 +0300

    трехзначное число с исключением средней цифры

--- sem2/s011/Program.cs
+++ sem2/s011/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+ int number = new Random().Next(100, 1000);
+ System.Console.WriteLine(number);
+
+int num1 = number/100;
+int num2 = number%10;
+System.Console.Write(num1);
+System.Console.Write(num2);
