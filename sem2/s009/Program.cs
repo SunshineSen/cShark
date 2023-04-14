68ac515b7b448134e11abbdeb4ab04694d5d3cf7
sen <kseniya2495@yandex.ru>
2023-04-14 16:55:43 +0300

    рандом 1099 через функцию

--- sem2/s009/Program.cs
+++ sem2/s009/Program.cs
@@ -1,13 +1,53 @@
 ﻿// случайное число из отрезка 10-99 и показывает наибольшую цифру числа
-int number = new Random().Next(10, 100);
-int first = number/10;
-int second = number % 10;
-System.Console.WriteLine(number);
-if (first > second)
+// без функции
+
+// int number = new Random().Next(10, 100);
+// int first = number/10;
+// int second = number % 10;
+// System.Console.WriteLine(number);
+// if (first > second)
+// {
+//    System.Console.WriteLine(first);
+// }
+// else
+// {
+//    System.Console.WriteLine(second);
+// }
+
+// решение через массив
+
+System.Console.WriteLine("min");
+int min = ReadInt();
+
+System.Console.WriteLine("max");
+int max = ReadInt();
+
+int number = GetRandomNumber(min, max);
+
+if (GetFirtDigit(number) > GetSecondDigit(number))
 {
-    System.Console.WriteLine(first);
+    System.Console.WriteLine("first");
 }
 else
 {
-    System.Console.WriteLine(second);
+    System.Console.WriteLine("second");
+}
+
+int GetRandomNumber(int min, int max)
+{
+    return new Random().Next(min, max + 1);
+}
+System.Console.WriteLine(number);
+
+int GetFirtDigit(int number)
+{
+    return number / 10;
+}
+int GetSecondDigit(int number)
+{
+    return number % 10;
+}
+int ReadInt()
+{
+    return int.Parse(Console.ReadLine());
 }
