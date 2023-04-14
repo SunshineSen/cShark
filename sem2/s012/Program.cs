f315d7eb393149ad19552f0e5203bb658ba2a1d6
sen <kseniya2495@yandex.ru>
2023-04-14 17:41:34 +0300

    кратное и остаток

---
+++ sem2/s012/Program.cs
@@ -0,0 +1,14 @@
+﻿// принимать на вход два числа, выводить является ли второе кратным первому. если число не кратно, выести остаток от деления
+Console.Write("введите числа ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = a % b;
+
+if (c == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("не кратно " + c);
+}
