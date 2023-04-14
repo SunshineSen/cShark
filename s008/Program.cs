e5c12d971c5bbb33131a94598969342ca74c5969
SunshineSen <128902151+SunshineSen@users.noreply.github.com>
2023-04-14 15:21:42 +0300

    Delete s008 directory

--- s008/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-Console.Write("введите число ");
-int j = int.Parse(Console.ReadLine());
- for (int i = 1; i <= j;  i++)
- {
-    if (i % 2 == 0)
-         Console.WriteLine(i+"");
- }
