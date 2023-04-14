3723ac3f71cfc8e99992c660aafdc1b86e493b24
sen <kseniya2495@yandex.ru>
2023-04-14 17:50:34 +0300

    кратное 7 и 23

---
+++ sem2/s014/Program.cs
@@ -0,0 +1,12 @@
+﻿// на вход число и прверяет кратно ли 7 и 23
+Console.Write("введите число ");
+int a = int.Parse(Console.ReadLine());
+
+if (a%7 == 0 & a%23 == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("не кратно ");
+}
