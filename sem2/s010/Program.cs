77e87099dfedd2ca9d498967b625dadc66ddbcf9
sen <kseniya2495@yandex.ru>
2023-04-15 16:05:29 +0300

    вывод второй цифры из трехзначного

---
+++ sem2/s010/Program.cs
@@ -0,0 +1,33 @@
+﻿// на вход трехзначное число и показывает вторую цифру
+
+ Console.Write("введите число ");
+int number = int.Parse(Console.ReadLine());
+if (number >= 100 && number < 1000)
+{
+    int a = number / 10 % 10;
+    Console.WriteLine($"вторая цифра '{a}'"); ;
+}
+else
+{
+    Console.WriteLine("неверное число");
+} 
+
+
+
+/* int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Введите число ");
+if (number < 100 | number >= 1000)
+{
+    System.Console.WriteLine("неверное число ");
+    return;
+}
+int a = number/10%10;
+System.Console.WriteLine($"вторая цифра '{a}'" ); 
+*/
