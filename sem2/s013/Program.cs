f1b2276f1947f5636c9164c603b9b7a6a02b7e36
sen <kseniya2495@yandex.ru>
2023-04-15 16:27:44 +0300

    вывод третьей цифры

---
+++ sem2/s013/Program.cs
@@ -0,0 +1,29 @@
+﻿//  выводит третью цифру числа или сообщает что ее нет
+int Numb(string message)
+{
+    System.Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+bool NotThree (int number)
+{
+    if (number < 100)
+    {
+        System.Console.WriteLine("третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+int ThirdNum (int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+int number = Numb("введите число ");
+if (NotThree (number))
+{
+    System.Console.WriteLine(ThirdNum(number));
+}
