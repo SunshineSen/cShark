a80ee37011bae4e63aae1d6b72f3d4ec28017f62
sen <kseniya2495@yandex.ru>
2023-04-15 16:42:28 +0300

    выходной

---
+++ sem2/s015/Program.cs
@@ -0,0 +1,35 @@
+﻿// принимает на вход цифру - день недели. и проверяет являктся ли он выходным
+int whatDay(string message)
+{
+    System.Console.Write(message);
+    int D = int.Parse(Console.ReadLine());
+    return D;
+}
+bool Weekend (int Day)
+{
+    if (Day > 5)
+    {
+        return true;
+    }
+    return false;
+}
+bool notDay (int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    System.Console.WriteLine("не день недели");
+    return false;
+}
+int weekDay = whatDay ("Введите день недели ");
+if (notDay(weekDay))
+{
+    if (Weekend(weekDay))
+    {
+        System.Console.WriteLine("выходной");
+    }
+    else {
+        System.Console.WriteLine("будний день");
+    }
+}
