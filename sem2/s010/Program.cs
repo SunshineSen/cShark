aed48e18b41c1454e270eb3be13abdb638aeb8a3
sen <kseniya2495@yandex.ru>
2023-04-15 16:15:00 +0300

    вторая цифра трехзначного числа

--- sem2/s010/Program.cs
+++ sem2/s010/Program.cs
@@ -1,28 +1,13 @@
 ﻿// на вход трехзначное число и показывает вторую цифру
 
- Console.Write("введите число ");
-int number = int.Parse(Console.ReadLine());
-if (number >= 100 && number < 1000)
-{
-    int a = number / 10 % 10;
-    Console.WriteLine($"вторая цифра '{a}'"); ;
-}
-else
-{
-    Console.WriteLine("неверное число");
-} 
-
-
-
-/* int Prompt(string message)
+int Numb(string message)
 {
     System.Console.Write(message);
-    string value = Console.ReadLine();
-    int result = Convert.ToInt32(value);
-    return result;
+    int a = int.Parse(Console.ReadLine());
+    return a;
 }
 
-int number = Prompt("Введите число ");
+int number = Numb("Введите число ");
 if (number < 100 | number >= 1000)
 {
     System.Console.WriteLine("неверное число ");
@@ -30,4 +15,4 @@ if (number < 100 | number >= 1000)
 }
 int a = number/10%10;
 System.Console.WriteLine($"вторая цифра '{a}'" ); 
-*/
+
