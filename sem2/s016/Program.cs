322a3bcb4287b8b8d4b18ee9aea75f468bd9c587
sen <kseniya2495@yandex.ru>
2023-04-15 15:08:53 +0300

    решение одно квадрат другого без функции

--- sem2/s016/Program.cs
+++ sem2/s016/Program.cs
@@ -1,2 +1,12 @@
 ﻿// на вход два числа и проверяет ялвяется ли одно квадратом другого
-Console.WriteLine("Hello, World!");
+Console.Write("введите числа ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a*a == b | b*b==a)
+{
+    System.Console.WriteLine("верно");
+}
+else{
+    System.Console.WriteLine("не верно");
+}
