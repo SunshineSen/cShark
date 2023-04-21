79e99e209f658ba90aa44ffba00479f210e8c01d
sen <kseniya2495@yandex.ru>
2023-04-22 02:05:18 +0300

    задача палиндром

---
+++ sem3/s019/Program.cs
@@ -0,0 +1,20 @@
+﻿// на вход пятизначное число, проверить палиндром или нет
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int Poly = number.Length;
+
+if (Poly == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - число - палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"введеное число не является пятизначным");
+}
