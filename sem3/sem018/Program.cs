c7a1448242354e289d9cce0e2b4d2f1a0ee0fd9b
sen <kseniya2495@yandex.ru>
2023-04-22 01:34:54 +0300

    диапазон координат

---
+++ sem3/sem018/Program.cs
@@ -0,0 +1,27 @@
+﻿// по заданному номеру четверти показывает диапазон возможных координат этой четверти
+void Quart ( int range)
+{
+    if (range == 1)
+    {
+        System.Console.WriteLine(" x: 1, y: inf");
+    }
+    else if (range == 2)
+    {
+        System.Console.WriteLine("x: -inf, y: inf");
+    }
+    else if (range == 3)
+    {
+        System.Console.WriteLine("x: -inf, y: 1");
+    }
+    else if (range == 4)
+    {
+        System.Console.WriteLine("x: inf, y: -inf");
+    }
+    else 
+    {
+        System.Console.WriteLine("это не четверть");
+    }
+}
+System.Console.WriteLine("введите число");
+int Q = int.Parse(Console.ReadLine());
+Quart((Q));
