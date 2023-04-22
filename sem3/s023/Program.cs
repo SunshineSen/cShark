2779b564236f11cda07185edd5d5ab6f80565d11
sen <kseniya2495@yandex.ru>
2023-04-22 09:00:04 +0300

    таблица кубов

---
+++ sem3/s023/Program.cs
@@ -0,0 +1,12 @@
+﻿// принимает число N и выводит таблицу квадратов от 1 до N
+void CubeT(int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        System.Console.WriteLine(" " + Math.Pow(i, 3));
+    }
+}
+
+System.Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+CubeT(N);
