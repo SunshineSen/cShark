e85bdd30d4c2e5e5091df46ccdd69e799aba8809
sen <kseniya2495@yandex.ru>
2023-04-23 01:00:49 +0300

    сумма от 1 до А

---
+++ sem4/s024/Program.cs
@@ -0,0 +1,12 @@
+﻿// принимает на вход число (А) и выдаёт сумму чисел от 1 до А
+int SummA (int A)
+{
+    int result = 0;
+    for (int i = 1; i <= A; i++)
+        result += i;
+    return result;
+
+}
+System.Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine (SummA(A));
