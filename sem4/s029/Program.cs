b86ed4438d052d3e9a9ce8eb79fd1bad2112258c
sen <kseniya2495@yandex.ru>
2023-04-23 10:57:52 +0300

    заполнение массива

---
+++ sem4/s029/Program.cs
@@ -0,0 +1,27 @@
+﻿// задаёт массив из 8 элементов и выводит их на экран.
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    Console.Write("[");
+    for (int index =0; index <collection.Length-1; index++)
+    {
+        System.Console.Write($"{collection[index]},");
+    }
+    System.Console.Write($"{collection[collection.Length-1]}");
+    Console.Write("]");
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
