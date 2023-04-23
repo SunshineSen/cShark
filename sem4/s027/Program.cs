3921b9b54320f8c859aa8048cb874adf923d58a1
sen <kseniya2495@yandex.ru>
2023-04-23 10:27:50 +0300

    сумма цифр в числе

---
+++ sem4/s027/Program.cs
@@ -0,0 +1,17 @@
+﻿// принимает на вход число и выдаёт сумму цифр в числе.
+int SumNumb(int number)
+{
+    int sum = 0;
+
+while (number > 0)
+{
+    sum+= number %10;
+    number = number /10;
+}
+return sum;
+}
+
+Console.WriteLine("введите число");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"сумма цифр в числе: {SumNumb(number)}");
