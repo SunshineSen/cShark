d9b8f4f2d46c95fb2f00cacaa4f8353b10022f7f
sen <kseniya2495@yandex.ru>
2023-04-23 09:26:25 +0300

    количество цифр в числе

---
+++ sem4/s026/Program.cs
@@ -0,0 +1,14 @@
+﻿// принимает на вход число и выдаёт количество цифр в числе.
+int Numb (int number)
+{
+    int count = 1;
+    while (number >9)
+    {
+        number/=10;
+        count++;
+    }
+    return count;
+}
+System.Console.WriteLine("введите число ");
+int N = int.Parse(Console.ReadLine());
+System.Console.WriteLine("количество элементов в числе:"+ Numb(N));
