93e13557b31783f09f7876092ec63baa2896e64b
sen <kseniya2495@yandex.ru>
2023-04-23 09:31:25 +0300

    произведение от 1 до N

---
+++ sem4/s028/Program.cs
@@ -0,0 +1,12 @@
+﻿// принимает на вход число N и выдаёт произведение чисел от 1 до N.
+int CompN (int N)
+{
+    int result = 1;
+    for (int i = 1; i <= N; i++)
+        result *= i;
+    return result;
+
+}
+System.Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine (CompN(N));
