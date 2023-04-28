1992531d93fa8c322038d0bf44512d9816eabbdb
sen <kseniya2495@yandex.ru>
2023-04-29 02:51:03 +0300

    массив. пвзница между мин макс

---
+++ sem5/s036/Program.cs
@@ -0,0 +1,31 @@
+﻿//  Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int size = 10;
+int[] numbers = new int[size];
+ArrayRandom (numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int i = 1; i < numbers.Length; i+=2)
+    sum = sum + numbers[i];
+
+    Console.WriteLine($"сумма элементов на нечётных позициях = {sum}");
+
+void ArrayRandom(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
