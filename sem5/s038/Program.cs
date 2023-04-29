9aa562e7736929d3a3997c199280d8aafbcee8d2
sen <kseniya2495@yandex.ru>
2023-04-29 10:30:59 +0300

    максимум, минимум, разница

---
+++ sem5/s038/Program.cs
@@ -0,0 +1,42 @@
+﻿// массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+
+int size = 10;
+double[] numbers = new double[size];
+ArrayRandom(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+}
+
+Console.WriteLine($"Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void ArrayRandom(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(10,100)) / 10;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
