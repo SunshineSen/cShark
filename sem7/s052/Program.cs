d32fcc53ad8612ac82fee2d2c06178d51ed1ac88
sen <kseniya2495@yandex.ru>
2023-05-06 00:06:40 +0300

    средн арефм. в каждом столбце

---
+++ sem7/s052/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int lineM = 4;
+int columN = 4;
+int[,] numbers = new int[lineM, columN];
+CreateTwoDimArray(numbers);
+PrintArray2D(numbers);
+
+double[] dNumbers = new double[numbers.GetLength(1)];
+
+for (int i = 0; i < numbers.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < numbers.GetLength(0); j++)
+    {
+        result += numbers[j, i];
+    }
+    dNumbers[i] = result / numbers.GetLength(0);
+}
+PrintArrayTwoDim(dNumbers);
+
+
+void CreateTwoDimArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void PrintArrayTwoDim(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
