ea14f08fdd81e641a073f83a32b3a247560b09d9
sen <kseniya2495@yandex.ru>
2023-05-24 21:38:42 +0300

    переворот 1 и последней строк

---
+++ sem8/s053/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задать двумерный массив. Программа которая поменяет местами первую ипосоледнюю строку массива
+int[,] CreateTwoDimArray(int firstLength, int secondLength)
+{
+    int[,] array = new int[firstLength, secondLength];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+
+    return array;
+}
+
+void ChangeRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int temp = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = temp;
+
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] array = CreateTwoDimArray(4, 4);
+PrintArray(array);
+ChangeRows(array);
+Console.WriteLine(String.Empty);
+PrintArray(array);
