6c53f7ee3d5ee08552c87042736adfa09ef3fc8a
sen <kseniya2495@yandex.ru>
2023-05-24 21:43:07 +0300

    поменять местами 1 и последнюю строки

--- sem8/s053/Program.cs
+++ sem8/s053/Program.cs
@@ -4,6 +4,7 @@ int[,] CreateTwoDimArray(int firstLength, int secondLength)
     int[,] array = new int[firstLength, secondLength];
 
     for (int i = 0; i < array.GetLength(0); i++)
+    
         for (int j = 0; j < array.GetLength(1); j++)
             array[i, j] = new Random().Next(0, 10);
 
