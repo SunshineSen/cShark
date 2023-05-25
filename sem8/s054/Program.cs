b8da340df42aed99ef5773dae23c116fb2bac20a
sen <kseniya2495@yandex.ru>
2023-05-25 22:26:47 +0300

    Двумерный массив.Упорядочить по убыванию элементы каждой строки двумерного массива.

---
+++ sem8/s054/Program.cs
@@ -0,0 +1,48 @@
+﻿//  Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int[,] CreateTwoDimArray(int firstLength, int secondLength)
+{
+    int[,] array = new int[firstLength, secondLength];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+
+    return array;
+}
+
+void ChangeElements(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int n = 0; n < array.GetLength(1) - 1; n++)
+            {
+                if (array[i, n] < array[i, n + 1])
+                {
+                    int temp = array[i, n + 1];
+                    array[i, n + 1] = array[i, n];
+                    array[i, n] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] array = CreateTwoDimArray(4, 4);
+PrintArray(array);
+ChangeElements(array);
+Console.WriteLine(String.Empty);
+PrintArray(array);
