7f85bb007affaf764d8805bbe8782e485ca2e5c3
sen <kseniya2495@yandex.ru>
2023-05-27 00:08:57 +0300

    рекруссия натуральные числа от 1 до Н

---
+++ sem9/s063/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N
+
+void PrintNumber(int number)
+{
+    if (number !=1)
+    {
+        PrintNumber(number-1);
+    }
+    Console.WriteLine(number);
+}
+PrintNumber(5);
+
