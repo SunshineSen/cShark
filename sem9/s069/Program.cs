172cde736032fb95879974486f2a6c702157791b
sen <kseniya2495@yandex.ru>
2023-05-27 15:56:47 +0300

    принимает два числа А и В и возводит число А в степень В с помощью рекурсии

---
+++ sem9/s069/Program.cs
@@ -0,0 +1,10 @@
+﻿// принимает два числа А и В и возводит число А в степень В с помощью рекурсии
+int PowNumber (int firstNumber, int secondNumber)
+{
+    if (secondNumber > 0)
+    {
+        return firstNumber * PowNumber(firstNumber, secondNumber -1);
+    }
+    return 1;
+}
+System.Console.WriteLine(PowNumber(2, 10));
