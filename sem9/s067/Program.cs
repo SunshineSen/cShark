d3ca98a1b365cb0f981c9c4975c146f2a2035ca6
sen <kseniya2495@yandex.ru>
2023-05-27 15:46:25 +0300

    программа принимает на вход число и возвращает сумму его цифр

---
+++ sem9/s067/Program.cs
@@ -0,0 +1,10 @@
+﻿// программа принимает на вход число и возвращает сумму его цифр
+int SumNun(int number)
+{
+    if (number > 0)
+    {
+        return number % 10 + SumNun(number/10);
+    }
+    return number% 10;
+}
+Console.WriteLine(SumNun(435));
